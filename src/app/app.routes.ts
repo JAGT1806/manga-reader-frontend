4ab17dca1685ce3d796584c4e2cd6dcfdfc2d2b4
JAGT <JAGT1806@gmail.com>
2024-10-19 11:28:29 -0500

    feat: add header with their routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    }, 
+    {
+        path: 'search',
+        component: SearchComponent
+    },
+    {
+        path: 'auth',
+        component: AuthComponent
+    }
+];
